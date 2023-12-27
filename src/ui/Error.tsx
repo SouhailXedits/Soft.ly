b8187ead0bc18c041ddd8e72c8ee85f611da31d7
souhail_edits <souhailbrahmi@takiacademyteam.com>
2023-12-27 12:23:55 +0100

    feat: adding the error page

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,9 +1,14 @@
 function Error() {
     return (
-        <div>
-            error
+      <div className="min-h-screen flex items-center justify-center bg-blue-500">
+        <div className="bg-white p-8 rounded-lg shadow-md text-center">
+          <h1 className="text-4xl font-bold mb-4 text-blue-500">Oops!</h1>
+          <p className="text-lg text-gray-700">
+            Something went wrong. Please try again later.
+          </p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Error
