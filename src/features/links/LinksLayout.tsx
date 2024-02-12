b5a19cdd7b0e2a739aa4d7012971867e94cf4088
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-02-12 11:34:39 +0100

    feat: add filter by date range in links

--- src/features/links/LinksLayout.tsx
+++ src/features/links/LinksLayout.tsx
@@ -3,35 +3,60 @@ import { useQuery } from "@tanstack/react-query";
 import ShortenedUrl from "./components/ShortenedUrl";
 import { request } from "graphql-request";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useUser } from "../auth/useUser";
 import { GQL_API_LINK } from "../../config";
 import { getUrls } from "../../services/apiLinks";
 import { ApiResponse } from "../../types";
-
-
+import { BsCalendar } from "react-icons/bs";
+import DateRangePicker from "@/ui/calendars/DateRangePicker";
+import DateRangePickerTwo from "@/ui/calendars/DateRangePickerTwo";
+// const { DatePicker, Space } = 'antd';
 
 function LinksLayout() {
-  const [ , setIsAllSelected] = useState(false);
+  const [, setIsAllSelected] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedLinks, setSelectedLinks] = useState<number[]>([]);
-  const {user} = useUser()
-  const userId = user?.id
+  const [filters, setFilters] = useState({
+    dateRange: {} as any,
+  });
+  const modalRef = useRef<any>(null);
+  const { user } = useUser();
+  const userId = user?.id;
   const { data, error } = useQuery<ApiResponse>({
     queryKey: ["urls", { user_id: userId }],
     queryFn: async ({ queryKey }) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
       const [, { user_id }] = queryKey;
-      const getUrlsQuery = getUrls
+      const getUrlsQuery = getUrls;
 
       return request(GQL_API_LINK, getUrlsQuery, {
         user_id,
       });
     },
   });
-  const allUrls = data?.getUrlsWithUserId || [];
 
-  if (error) console.error(error.message);
+  const [filteredUrls, setFilteredUrls] = useState<any[]>([]);
+  const [isFilterApplied, setIsFilterApplied] = useState(false);
+
+  useEffect(() => {
+    if (data && isFilterApplied) {
+      // Filter URLs based on date range
+      const filteredData = data.getUrlsWithUserId.filter((url: any) => {
+        const createdAt = new Date(url.created_at);
+        const fromDate = new Date(filters.dateRange.from);
+        const toDate = new Date(filters.dateRange.to);
+        // Adjusting the end date to include the full day
+        toDate.setDate(toDate.getDate() + 1);
+        return createdAt >= fromDate && createdAt < toDate; // Use '<' instead of '<='
+      });
+      setFilteredUrls(filteredData);
+    } else {
+      setFilteredUrls(data?.getUrlsWithUserId || []);
+    }
+  }, [data, filters, isFilterApplied]);
+  console.log(filters)
 
   const handleLinkSelection = (linkId: number) => {
     const updatedSelectedLinks = [...selectedLinks];
@@ -44,12 +69,81 @@ function LinksLayout() {
     }
 
     setSelectedLinks(updatedSelectedLinks);
-    setIsAllSelected(updatedSelectedLinks.length === allUrls?.length);
+    setIsAllSelected(updatedSelectedLinks.length === filteredUrls?.length);
   };
+
+  const handleFilterByDateClick = () => {
+    setIsFilterApplied(false);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsFilterApplied(true);
+    setIsModalOpen(false);
+  };
+
+  const handleOverlayClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      setIsModalOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.addEventListener("mousedown", handleOverlayClick);
+    } else {
+      document.removeEventListener("mousedown", handleOverlayClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOverlayClick);
+    };
+  }, [isModalOpen]);
+
+  function handleChangeDateRange(newDate: any) {
+    setFilters({
+      ...filters,
+      dateRange: newDate,
+    });
+  }
+
+  function handleApplyFilters() {
+    setIsFilterApplied(true);
+    closeModal();
+  }
 
   return (
     <div className="flex flex-col items-center text-center gap-5 px-4 py-9  w-full max-w-[70rem] mx-auto h-screen">
-      {allUrls?.length !== 0 ? (
+      <div className=" flex gap-2 justify-start w-full pb-5 border-b-2 ">
+        <button
+          onClick={handleFilterByDateClick}
+          className=" p-2 border rounded flex items-center gap-1 bg-white "
+        >
+          {" "}
+          <BsCalendar /> Filter By Created date
+        </button>
+        {/* <button className=" p-2 border rounded flex items-center gap-1 bg-white">
+          {" "}
+          <BsCalendar /> Add filters
+        </button> */}
+      </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center">
+          <div ref={modalRef} className="bg-white p-6 rounded-lg">
+            {/* Modal content goes here */}
+            <h2>Filter by date :</h2>
+            <DateRangePicker setdate={handleChangeDateRange} defaultSelected={filters.dateRange} />
+            <button
+              className=" p-2 bg-blue-700 hover:bg-blue-400 transition-all text-white rounded"
+              onClick={handleApplyFilters}
+            >
+              apply filters
+            </button>
+            {/* <button onClick={closeModal}>Close Modal</button> */}
+          </div>
+        </div>
+      )}
+
+      {filteredUrls?.length !== 0 ? (
         <div className="flex items-center justify-between w-full ">
           <h2 className="text-3xl font-bold">Links</h2>
         </div>
@@ -57,8 +151,8 @@ function LinksLayout() {
         ""
       )}
 
-      {allUrls?.length !== 0 ? (
-        allUrls?.map(
+      {filteredUrls?.length !== 0 &&
+        filteredUrls.map(
           (Newlink: {
             id: number;
             created_at: string;
@@ -74,8 +168,8 @@ function LinksLayout() {
               onSelect={handleLinkSelection}
             />
           )
-        )
-      ) : (
+        )}
+      {filteredUrls?.length === 0 && (
         <>
           <img
             className=" max-w-[400px] sm:max-w-[300px]"
---
+++ src/ui/calendars/DateRangePicker.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+import { addDays, format } from "date-fns";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
+const pastMonth = Date.now();
+
+export default function DateRangePicker({ setdate, defaultSelected }: any) {
+  // const defaultSelected: DateRange = {
+  //   from: pastMonth,
+  //   to: addDays(pastMonth, 4),
+  // };
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  useEffect(() => {
+    // Only call setdate when range changes
+    if (range) {
+      setdate(range);
+    }
+  }, [range]);
+  // setdate(range);
+  const css = `
+  .rdp-day_selected { 
+    background-color: #0000ff;
+    color: white
+  }
+
+  .my-today { 
+    font-weight: bold;
+    font-size: 140%; 
+    color: red;
+  }
+`;
+
+  let footer = <p>Please pick the first day.</p>;
+  if (range?.from) {
+    if (!range.to) {
+      footer = <p>{format(range.from, "PPP")}</p>;
+    } else if (range.to) {
+      footer = (
+        <p>
+          {format(range.from, "PPP")}–{format(range.to, "PPP")}
+        </p>
+      );
+    }
+  }
+
+  return (
+    <>
+      <style>{css}</style>
+
+      <DayPicker
+        id="test"
+        mode="range"
+        // defaultMonth={pastMonth}
+        selected={range}
+        footer={footer}
+        onSelect={setRange}
+      />
+    </>
+  );
+}
